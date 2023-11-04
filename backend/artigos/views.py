573eba32073a73a2c7b837eb8766a802af0c8854
Daniel Menezes <danielmenezes42@gmail.com>
2023-11-04 10:47:33 -0300

    Cria estrutura das views dos artigos

--- backend/artigos/views.py
+++ backend/artigos/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from artigos.models import Autor, Artigo
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+from rest_framework.decorators import api_view
+from rest_framework.decorators import permission_classes
+from rest_framework.decorators import authentication_classes
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+class ArtigoView(APIView):
+
+    @swagger_auto_schema()
+    def post(self, request):
+        pass
+    
+    @swagger_auto_schema()
+    def get(self, request, id_arg):
+        pass
+
+    @swagger_auto_schema()
+    def put(self, request, id_arg):
+        pass
+
+
+class ArtigosView(APIView):
+    
+    @swagger_auto_schema()
+    def get(self, request):
+        pass
+
+    @api_view(('DELETE',))
+    @authentication_classes([TokenAuthentication])
+    @permission_classes([IsAuthenticated])
+    @swagger_auto_schema(
+    )
+    def delete(self, request):
+        pass
+
+class AutorView(APIView):
+    @swagger_auto_schema()
+    def get(self, request):
+        pass
+
+    @swagger_auto_schema()
+    def post(self, request):
+        pass
+
+    @swagger_auto_schema()
+    def put(self, request):
+        pass
+
+    @swagger_auto_schema()
+    def delete(self, request):
+        pass
+    
+class AutoresView(APIView):
+    @swagger_auto_schema()
+    def get(self, request):
+        pass
