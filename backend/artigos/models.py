3a608b39d3056df7e94bc93fbd98b7970f279fa9
Daniel Menezes <danielmenezes42@gmail.com>
2023-11-04 10:33:06 -0300

    Cria models do app artigos

--- backend/artigos/models.py
+++ backend/artigos/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Autor(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=255, help_text='Insira o nome do autor',
+                            verbose_name='Nome do autor*')
+    
+    class Meta:
+        ordering = ["nome"]
+        
+    def __str__(self):
+        return self.nome
+    
+
+class Artigo(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=255, 
+                            help_text='Insira o título do artigo',
+                            verbose_name='Título do artigo*')
+    ano_publicacao = models.CharField(max_length=4, 
+                                      help_text='Insira o ano de publicação' +
+                                       ' do artigo',
+                                      verbose_name='Ano de publicação*',
+                        validators=[MinLengthValidator(4,
+                            'Campo precisa ter 4 dígitos.')])
+    autores = models.ManyToManyField(Autor, help_text="Para selecionar mais" +
+                                     " de um autor, segure" +
+                                     " a tecla Ctrl ao clicar.",
+                                     verbose_name="Autores*")
+    link = models.URLField(blank=True,
+            verbose_name='Link',
+            help_text= 'Insira o link para o artigo (Opcional)')
+
+    class Meta:
+        ordering = ["nome"]
+        
+    def __str__(self):
+        return self.nome
+
