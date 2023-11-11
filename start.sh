3dc49372424e4972137739ac5ccea2ce3bf6b931
Daniel Menezes <danielmenezes42@gmail.com>
2023-11-11 13:29:48 -0300

    adiciona informaçoes de debug do deploy

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+ls -la >&2
+pwd >&2
+whoami >&2
+
+
+# Stop all containers
 docker-compose -f docker-compose.prod.yml down
+# Build all containers
 docker-compose -f docker-compose.prod.yml up -d --build
