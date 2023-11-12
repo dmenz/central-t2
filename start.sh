1556d184b2832cc6a76ae2e84711f906565d2e2d
Daniel Menezes <danielmenezes42@gmail.com>
2023-11-12 01:08:50 -0300

    certificados ssl

--- start.sh
+++ start.sh
@@ -2,6 +2,9 @@
 
 cd /home/ubuntu/central-t2
 
+cp frontend/nginx.prod.conf frontend/nginx.conf
+cp backend/nginx.prod.conf backend/nginx.conf
+
 # Stop all containers
 docker-compose -f docker-compose.prod.yml down
 # Build all containers
